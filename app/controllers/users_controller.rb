c4e5d49499f4868667d8a4185a0400be65affe89
midori-panisa <64828177+midori-panisa@users.noreply.github.com>
2020-05-15 22:14:33 +0900

    add-edit-new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
 
-    private
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
