8c393a8dc283d111128edb1888c0bc42259d02b4
midori-panisa <64828177+midori-panisa@users.noreply.github.com>
2020-05-16 18:54:24 +0900

    Revert "メッセージ機能の追加"

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,7 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:edit, :update]
   def index
   end
-
+  
   def new
     @group = Group.new
     @group.users << current_user
@@ -25,7 +24,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
+      redirect_to root_path, notice: 'グループを更新しました'
     else
       render :edit
     end
@@ -35,8 +34,5 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
-  end
-  def set_group
-    @group = Group.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :users, only: [:edit, :update, :index]
-  resources :groups, only: [:index, :new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
-  end
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
