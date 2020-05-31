071f249ff57fd08be90c62011b3d16c0a10136b3
midori-panisa <64828177+midori-panisa@users.noreply.github.com>
2020-05-31 16:37:03 +0900

    change-deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock '3.14.0'
 
-set :application, "chat-space"
-set :repo_url, "git@example.com:midori-panisa/chat-space.git"
+set :application, 'chat-space'
+set :repo_url, 'git@example.com:midori-panisa/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -49,7 +49,7 @@ end
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-set :keep_releases, 5
+# set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
